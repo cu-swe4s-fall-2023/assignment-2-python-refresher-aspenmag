c671a974248864f7cfa2d05d854d72c0fa67db1e
Aspen <asda9499@colorado.edu>
2023-09-12 22:37:00 +0000

    query file for value, return column

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,9 @@
+import numpy as np
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    value_match = []
+    with open(file_name,'r') as f:
+        for line in f:
+            line_data = line.split(',')
+            if line_data[query_column] == query_value:
+                value_match.append(line_data[result_column])
+    return(value_match)
---
+++ run.sh
@@ -0,0 +1,6 @@
+import my_utils as u
+
+agro_dat = 'Agrofood_co2_emission.csv'
+
+ans = u.get_column(agro_dat, 2,'1557.583', 0)
+print(ans)
