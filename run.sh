e80dcde4b8a5be6b65133e07303e24a308dfd339
Aspen <asda9499@colorado.edu>
2023-09-24 19:51:53 +0000

    update to use command line arguments to call print_fires

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-python print_fires.py
+#1: runs correctly
+python print_fires.py --filename Agrofood_co2_emission.csv  --country Albania  --country_column 0  --fires_column 1
+
+#2: produces type error
+python print_fires.py --filename Agrofood_co2_emission.csv  --country Albania --country_column 0  --fires_column Albania
+
+#3: produces out of range error
+python print_fires.py --filename Agrofood_co2_emission.csv  --country Albania --country_column 500  --fires_column 1
